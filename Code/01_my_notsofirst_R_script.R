2cb146aad16e1c9e6a1ef510b186ba01113c41b8
paolapert <pertusi74@gmail.com>
2025-10-07 17:33:18 +0200

    Create 01_my_notsofirst_R_script.R

---
+++ Code/01_my_notsofirst_R_script.R
@@ -0,0 +1,21 @@
+# without an hashtag the comment can be understood by R when i paste in in R. The comment is used to DESCRIBE the function. Its just to let you understand what the function means!!! After, you can put the script
+2+3
+
+naza <- 2+3
+
+marco <- 7+4
+
+naza + marco
+
+vale <- c(5,10,13,20,30)
+
+elia <- c(2,5,10,40,70)
+
+
+plot(elia,vale)
+
+# if i want to change the point symbol i will use pch which means point character
+plot(elia,vale,pch=19)
+
+#to change the color i will use col, to change the dimension i will use cex
+ploy(elia,vale,pch=19,col="blue",cex=2)
